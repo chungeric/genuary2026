162a045b270d9fe6dd5b69dd8d2b09f4d6a82fb1
Eric Chung <rchung.eric@gmail.com>
2025-12-30 18:37:04 +1100

    Day 2 base idea

--- 2/src/main.js
+++ 2/src/main.js
@@ -1,65 +1,75 @@
-import * as THREE from 'three';
-// import * as THREE from 'three/webgpu'
+// Inspiration: https://observablehq.com/@tarte0/animation-curves-with-three-js
+
+import * as THREE from 'three/webgpu'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-// import { Fn, sin, uniform, uv, vec4, clamp } from 'three/tsl'
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.z = 5;
+scene.background = new THREE.Color(0x000000);
+const camera = new THREE.PerspectiveCamera( 20, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.x = 20;
+camera.position.y = 20;
+camera.position.z = 20;
 
-// --- WebGL Renderer ---
-const renderer = new THREE.WebGLRenderer();
+// --- WebGPU Renderer  ---
+const renderer = new THREE.WebGPURenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
-// --- WebGPU Renderer  ---
-// const renderer = new THREE.WebGPURenderer();
-// renderer.setSize( window.innerWidth, window.innerHeight );
-// renderer.setAnimationLoop( animate );
-// document.body.appendChild( renderer.domElement );
-
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 1.2, 0);
 
-const geometry = new THREE.PlaneGeometry( 1, 1, 8, 8 );
-
-// --- WebGL Shader Material Example ---
-const material = new THREE.ShaderMaterial({
-  vertexShader: `
-    varying vec2 vUv;
-    void main() {
-      vUv = uv;
-      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-    }
-  `,
-  fragmentShader: `
-    varying vec2 vUv;
-    uniform float time;
-    void main() {
-      gl_FragColor = vec4(vUv, 0.0, sin(time * 0.5) * 0.5 + 0.5);
-    }
-  `,
-  uniforms: {
-    time: { value: 0.0 }
-  },
-  side: THREE.DoubleSide,
-  transparent: true,
-  wireframe: true
-});
-
-//  --- WebGPU Node Material Example ---
-// const material = new THREE.MeshBasicNodeMaterial({ color: 0x00ff00, side: THREE.DoubleSide, transparent: true, wireframe: true });
+const ballGeometry = new THREE.SphereGeometry( 0.5, 32, 32 );
+const ballMaterial = new THREE.MeshBasicNodeMaterial({ color: 0xff0000, side: THREE.DoubleSide });
 // let time = uniform(0);
 // material.colorNode = Fn(() => {
 //   return vec4(uv(), 0, sin(time.mul(0.5)).mul(0.5).add(0.5));
 // })();
 
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const floorGeometry = new THREE.PlaneGeometry( 1.2, 1.2 );
+const floorMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff, side: THREE.DoubleSide } );
+
+const gridItems = [];
+const GRID_SIZE = 1;
+
+function createGrid() {
+  for (let i = -GRID_SIZE; i <= GRID_SIZE; i++) {
+    for (let j = -GRID_SIZE; j <= GRID_SIZE; j++) {
+
+      const gridItem = new THREE.Group();
+
+      const ballGroup = new THREE.Group();
+      const ball = new THREE.Mesh( ballGeometry, ballMaterial );
+      ball.position.x = i * 2;
+      ball.position.y = 0.5;
+      ball.position.z = j * 2;
+      ballGroup.add( ball );
+      ballGroup.userData.timeScale = 2;
+      ballGroup.userData.jumpHeight = 2;
+      ballGroup.userData.squishTo = 0.4;
+      ballGroup.userData.offset = Math.random() * 1000;
+      gridItem.add( ballGroup );
+
+      const floor = new THREE.Mesh( floorGeometry, floorMaterial );
+      floor.rotation.x = - Math.PI / 2;
+      floor.position.x = i * 2;
+      floor.position.z = j * 2;
+      floor.position.y = 0.5;
+      gridItem.add( floor );
+
+      gridItems.push( gridItem );
+
+      scene.add( gridItem );
+    }
+  }
+}
+
+createGrid();
 
 //  --- Lights ---
-// const pointLight = new THREE.PointLight(0xffffff, 80);
+// const pointLight = new THREE.PointLight(0xffffff, 10);
 // pointLight.position.set(2, 2, 2);
+// pointLight.castShadow = true;
 // scene.add(pointLight);
 // const pointLight2 = new THREE.PointLight(0xffffff, 80);
 // pointLight2.position.set(-2, -2, -2);
@@ -71,9 +81,26 @@ window.addEventListener('resize', () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
+const clock = new THREE.Clock();
 function animate() {
-  // time.value += 0.1;
-  // material.uniforms.time.value += 0.1;
+  gridItems.forEach((gridItem) => {
+    const ballGroup = gridItem.children[0];
+    const floor = gridItem.children[1];
+    const jumpHeight = ballGroup.userData.jumpHeight;
+    const squishTo = ballGroup.userData.squishTo;
+    const timeScale = ballGroup.userData.timeScale;
+    const offset = ballGroup.userData.offset;
+    const t = Math.abs(Math.sin((clock.getElapsedTime() + offset) * timeScale)) * jumpHeight;
+    ballGroup.position.y = t;
+    if (t < 0.5) {
+      ballGroup.scale.y = THREE.MathUtils.mapLinear(t, 0.5, 0, 1, squishTo);
+      floor.position.y = THREE.MathUtils.mapLinear(t, 0.5, 0, 0.5, 0);
+    } else {
+      ballGroup.scale.y = 1;
+      floor.position.y = 0.5;
+    }
+  });
+
   controls.update();
   renderer.render( scene, camera );
 }
