8b1f006aba0fb81b0488523322b3af9764d4bddf
Eric Chung <rchung.eric@gmail.com>
2025-12-30 13:03:05 +1100

    Test hard coded vite config base

--- 1/vite.config.js
+++ 1/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 
 export default defineConfig(({ mode }) => {
   return {
-    base: mode === 'production' ? '/genuary2026/1/' : '/',
+    base: '/genuary2026/1/',
   }
 })
